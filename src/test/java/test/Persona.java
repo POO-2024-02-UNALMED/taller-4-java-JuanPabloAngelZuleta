842406f815573e0abac0e5b99ad6c96447c692cc
Juan Angel <juanpabloangelzuleta@gmail.com>
2024-11-25 13:03:29 -0500

    taller4.2

--- src/test/java/test/Persona.java
+++ src/test/java/test/Persona.java
@@ -16,25 +16,25 @@ public class Persona {
     }
     
     public Persona(long cedula, String nombre) {
-        this.cedula = cedula;
-        this.nombre = nombre;
+        this.cedula=cedula;
+        this.nombre=nombre;
         totalPersonas++;
     }
     
     public Persona(String nombre, long cedula) {
-        this.cedula = cedula;
-        this.nombre = nombre;
+        this.cedula=cedula;
+        this.nombre=nombre;
         totalPersonas++;
     }
 
     public Persona(long cedula) {
-        this.cedula = cedula;
-        this.nombre = "";
+        this.cedula=cedula;
+        this.nombre="";
         totalPersonas++;
     }
 
     public Persona(String nombre) {
-        this.nombre = nombre;
+        this.nombre=nombre;
         this.cedula=1;
         totalPersonas++;
     }
