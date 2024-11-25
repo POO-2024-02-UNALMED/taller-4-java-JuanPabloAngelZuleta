307dd8590a61c29d29deb5f35c05d611d7dbacde
Juan Angel <juanpabloangelzuleta@gmail.com>
2024-11-25 13:20:26 -0500

    taller4.3

--- src/test/java/test/Persona.java
+++ src/test/java/test/Persona.java
@@ -1,44 +1,43 @@
-package test;
+package classroom;
 
 public class Persona {
 
     final long cedula;
     String nombre;
     static int totalPersonas;
-    
+
     static {
         totalPersonas = 0;
     }
 
-    public Persona() {
-    	this.cedula=0;
-        totalPersonas++;
-    }
-    
     public Persona(long cedula, String nombre) {
-        this.cedula=cedula;
-        this.nombre=nombre;
+        this.cedula = cedula;
+        this.nombre = nombre;
         totalPersonas++;
     }
-    
+
     public Persona(String nombre, long cedula) {
-        this.cedula=cedula;
-        this.nombre=nombre;
+        this.cedula = cedula;
+        this.nombre = nombre;
         totalPersonas++;
     }
 
     public Persona(long cedula) {
-        this.cedula=cedula;
-        this.nombre="";
+        this.cedula = cedula;
+        this.nombre = "";
         totalPersonas++;
     }
 
     public Persona(String nombre) {
-        this.nombre=nombre;
-        this.cedula=1;
+        this.nombre = nombre;
+        this.cedula = 1;
         totalPersonas++;
     }
-    
+
+    public Persona() {
+        this.cedula = 0;
+    }
+
     public long getCedula() {
         return cedula;
     }
