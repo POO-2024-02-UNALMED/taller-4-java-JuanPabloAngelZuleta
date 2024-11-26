bdadbd1ca893faa801614453aed296faac59ff33
AZ86LOL <juanpabloangelzuleta@gmail.com>
2024-11-26 10:09:44 -0500

    taller4.6

--- src/main/java/classroom/Asignatura.java
+++ src/main/java/classroom/Asignatura.java
@@ -1,51 +1,50 @@
 package classroom;
-public class Asignatura {
 
+public class Asignatura {
     public String nombre;
     String n = nombre;
     public int codigoInterno;
     public int codigoExterno;
     public Tipo tipo;
-
+    
+    
     public Asignatura() {
         this("Sin nombre",Tipo.FUNDAMENTACION);
     }
-
     public Asignatura(int codigoExterno,Tipo tipo) {
         this("Sin nombre", 0, codigoExterno,tipo);
     }
-
     public Asignatura(String nombre, Tipo tipo) {
         this(nombre, 0, 0,tipo);
     }
-
     public Asignatura(String nombre, int codigoInterno, int codigoExterno,Tipo tipo) {
-        this.nombre = nombre;
-        this.codigoInterno = codigoInterno;
-        this.codigoExterno = codigoExterno;
-        this.tipo = tipo;
+        this.nombre=nombre;
+        this.codigoInterno=codigoInterno;
+        this.codigoExterno=codigoExterno;
+        this.tipo=tipo;
     }
-
+    
+    
     public void cambiarDatos(int codigoInterno, int codigoExterno, String nombre,Tipo tipo) {
-        this.codigoInterno = codigoInterno;
-        this.codigoExterno = codigoExterno;
-        this.nombre = nombre;
-        this.tipo = tipo;
+        this.codigoInterno=codigoInterno;
+        this.codigoExterno=codigoExterno;
+        this.nombre=nombre;
+        this.tipo=tipo;
     }
-
     public void cambiarDatos(int codigoExterno) {
-        this.codigoExterno = codigoExterno;
+        this.codigoExterno=codigoExterno;
     }
-
     public void cambiarDatos(String nombre) {
-        this.nombre = nombre;
+        this.nombre=nombre;
     }
-
+    
+    
     public void setCodigoInterno(int codigoInterno) {
-        this.codigoInterno = codigoInterno;
+        this.codigoInterno=codigoInterno;
     }
-
     public void setCodigoInterno(double codigoInterno) {
-        this.codigoInterno = (int) codigoInterno;
+        this.codigoInterno=(int) codigoInterno;
     }
+    
+    
 }
--- src/main/java/classroom/Grupo.java
+++ src/main/java/classroom/Grupo.java
@@ -1,35 +1,34 @@
 package classroom;
 
 public class Grupo {
-
     public Persona[] estudiantes;
     public Persona profesor;
     public Asignatura asignatura;
     public int codigo;
     public String horario;
-
+    
+    
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
-        this.estudiantes = estudiantes;
-        this.profesor = profesor;
-        this.asignatura = asignatura;
-        this.codigo = codigo;
-        this.horario = horario;
+        this.estudiantes=estudiantes;
+        this.profesor=profesor;
+        this.asignatura=asignatura;
+        this.codigo=codigo;
+        this.horario=horario;
     }
-
     public Grupo(int cantidadEstudiantes, Persona profesor, Asignatura asignatura, int codigo, String horario) {
         this(new Persona[cantidadEstudiantes], profesor, asignatura, codigo, horario);
     }
-
     public Grupo(Persona[] estudiantes, Persona profesor, Asignatura asignatura) {
-        this.estudiantes = estudiantes;
-        this.profesor = profesor;
-        this.asignatura = asignatura;
+        this.estudiantes=estudiantes;
+        this.profesor=profesor;
+        this.asignatura=asignatura;
     }
-
+    
+    
     void cambiarEstudiante(Persona estudianteViejo, Persona estudianteNuevo) {
-        for (int i = 0; i < estudiantes.length; i++) {
-            if (estudiantes[i].getCedula() == estudianteViejo.getCedula()) {
-                estudiantes[i] = estudianteNuevo;
+        for (int x=0; x<estudiantes.length; x++) {
+            if (estudiantes[x].getCedula()==estudianteViejo.getCedula()) {
+                estudiantes[x]=estudianteNuevo;
                 break;
             }
         }
@@ -37,4 +36,6 @@ public class Grupo {
     void cambiarEstudiante(int indice, Persona estudiante) {
         estudiantes[indice] = estudiante;
     }
+    
+    
 }
